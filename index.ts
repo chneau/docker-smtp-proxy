84ab0ae9810fba29f95f746ef1671f771d1130f0
chneau <charles63500@gmail.com>
2024-03-14 18:39:50 +0000

    Add types for request and response bodies

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { swagger } from "@elysiajs/swagger";
 import { logger } from "@grotto/logysia";
 import { Elysia, t } from "elysia";
 import { createTransport } from "nodemailer";
+import { bodyType, errorType, headersType, responseType } from "./types";
 
 const port = Number.parseInt(process.env.PORT || "3000", 10);
 console.log(`PORT: ${port}`);
@@ -14,21 +15,6 @@ const r = new Elysia();
 r.use(logger({ logIP: true }));
 r.use(swagger());
 r.use(serverTiming());
-
-const allowedPrioritiesType = [
-	t.Literal("high"),
-	t.Literal("normal"),
-	t.Literal("low"),
-];
-
-const evenlopeType = t.Object({
-	from: t.Union([t.String(), t.Literal(false)]),
-	to: t.Array(t.String()),
-});
-
-const addressesType = t.Array(
-	t.Union([t.String(), t.Object({ name: t.String(), address: t.String() })]),
-);
 
 r.post(
 	"/",
@@ -43,7 +29,6 @@ r.post(
 			from: body.from,
 			to: body.to,
 			bcc: body.bcc,
-			priority: body.priority,
 			subject: body.subject,
 			text: body.text,
 			html: body.html,
@@ -55,48 +40,10 @@ r.post(
 		return result;
 	},
 	{
-		headers: t.Object({
-			"x-api-key": t.String(),
-		}),
-		response: {
-			200: t.Object({
-				envelope: evenlopeType,
-				messageId: t.String(),
-				accepted: addressesType,
-				rejected: addressesType,
-				pending: addressesType,
-				response: t.String(),
-			}),
-			400: t.Object({
-				name: t.String(),
-				message: t.String(),
-			}),
-		},
+		headers: headersType,
+		response: { 200: responseType, 400: errorType },
 		type: "application/json",
-		body: t.Object(
-			{
-				host: t.String(),
-				port: t.Optional(t.Numeric({ default: 25 })),
-				secure: t.Optional(t.Boolean({ default: false })),
-				from: t.String(),
-				to: t.String(),
-				bcc: t.Optional(t.String()),
-				priority: t.Optional(t.Union(allowedPrioritiesType)),
-				subject: t.String(),
-				text: t.Optional(t.String()),
-				html: t.String(),
-				attachments: t.Optional(
-					t.Array(
-						t.Object({
-							filename: t.String(),
-							content: t.String(),
-							encoding: t.Optional(t.String({ default: "base64" })),
-						}),
-					),
-				),
-			},
-			{ required: ["host", "from", "to", "subject", "html"] },
-		),
+		body: bodyType,
 		detail: {
 			summary: "Send an email",
 			description: "Send an email using the provided parameters",
---
+++ types.ts
@@ -0,0 +1,89 @@
+import { t } from "elysia";
+
+const allowedPrioritiesType = [
+	t.Literal("high"),
+	t.Literal("normal"),
+	t.Literal("low"),
+];
+
+const evenlopeType = t.Object({
+	from: t.Union([t.String(), t.Literal(false)]),
+	to: t.Array(t.String()),
+});
+
+const addressesType = t.Array(
+	t.Union([t.String(), t.Object({ name: t.String(), address: t.String() })]),
+);
+
+export const responseType = t.Object(
+	{
+		envelope: {
+			...evenlopeType,
+			description:
+				"The envelope information. This is the information that is used to generate the Message-Id and Received headers.",
+		},
+		messageId: t.String({ description: "The unique message identifier" }),
+		accepted: {
+			...addressesType,
+			description: "An array of addresses that were accepted for delivery",
+		},
+		rejected: {
+			...addressesType,
+			description: "An array of addresses that were rejected for delivery",
+		},
+		pending: {
+			...addressesType,
+			description: "An array of addresses that were temporarily rejected",
+		},
+		response: t.String({
+			description: "The last SMTP response from the server",
+		}),
+	},
+	{
+		description: "The email was sent successfully",
+	},
+);
+
+export const errorType = t.Object(
+	{
+		name: t.String({ description: "The error name" }),
+		message: t.String({ description: "The error message" }),
+	},
+	{ description: "Bad Request" },
+);
+
+export const bodyType = t.Object(
+	{
+		host: t.String({ description: "The host" }),
+		port: t.Optional(t.Numeric({ default: 25, description: "The port" })),
+		secure: t.Optional(t.Boolean({ default: false, description: "Secure" })),
+		from: t.String({ description: "The FROM" }),
+		to: t.String({ description: "The TO" }),
+		bcc: t.Optional(t.String({ description: "The BCC" })),
+		subject: t.String({ description: "The subject" }),
+		text: t.Optional(t.String({ description: "The text content" })),
+		html: t.String({ description: "The HTML content" }),
+		attachments: t.Optional(
+			t.Array(
+				t.Object(
+					{
+						filename: t.String({ description: "The filename" }),
+						content: t.String({ description: "The content" }),
+						encoding: t.Optional(
+							t.String({ default: "base64", description: "The encoding" }),
+						),
+					},
+					{ description: "The attachment" },
+				),
+			),
+		),
+	},
+	{ description: "The email parameters" },
+);
+
+export const headersType = t.Object(
+	{
+		"x-api-key": t.String({ description: "The API key" }),
+	},
+	{ description: "The headers" },
+);
